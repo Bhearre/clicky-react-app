d3166d607f8915f5b0c659ac599b312b44f60be9
George Ferguson <george@fergusondevelopmentgroup.com>
2020-03-20 21:05:12 -0400

    Updates

---
+++ precache-manifest.521a4234fa2075181fc4e08020e23605.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0182eb65b551f0446298a748a165b174",
+    "url": "/clicky-react-app/index.html"
+  },
+  {
+    "revision": "2d7f69b48f9112d036e4",
+    "url": "/clicky-react-app/static/css/main.acace3ea.chunk.css"
+  },
+  {
+    "revision": "1f79317fcdd568995067",
+    "url": "/clicky-react-app/static/js/2.36ca9fe4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clicky-react-app/static/js/2.36ca9fe4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d7f69b48f9112d036e4",
+    "url": "/clicky-react-app/static/js/main.577bf457.chunk.js"
+  },
+  {
+    "revision": "4768460d3cb609277cdc",
+    "url": "/clicky-react-app/static/js/runtime-main.8fd4e4a5.js"
+  }
+]);
